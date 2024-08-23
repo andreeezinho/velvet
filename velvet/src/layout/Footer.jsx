11ea24b2bb783d1bd6bfd85481780897c717d592
andreeezinho <andreeesapucaia@gmail.com>
2024-08-23 17:39:03 -0300

    estilizando footer

--- velvet/src/layout/Footer.jsx
+++ velvet/src/layout/Footer.jsx
@@ -1,6 +1,31 @@
+import styles from "../css/Footer.module.css";
+
+import { FaInstagram, FaWhatsapp, FaFacebook } from "react-icons/fa";
+
 function Footer() {
     return(
-        <footer></footer>
+        <footer className={styles.footer}> 
+            <div className={styles.container}>
+                <div className={styles.logo}>
+                    <h1><span>V</span>elvet</h1>
+                </div>
+                
+                <div className={styles.tabela}>
+                    <ul>
+                        <p>Empresa</p>
+                        <li>Nossos produtos</li>
+                        <li>Contate-nos</li>
+                        <li>Sobre nós</li>
+                    </ul>
+                    <ul className={styles.sociais}>
+                        <p>Redes Socias</p>
+                        <li><FaFacebook /> Facebook</li>
+                        <li><FaInstagram/> Instagram</li>
+                        <li><FaWhatsapp /> Whatsapp</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     );
 }
 
